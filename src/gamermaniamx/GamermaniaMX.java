7245f4b7ea46224f5aef6c96af83703bf382ac1a
Mao Rivera <Mao Rivera@ba-dd2dbde06924>
2016-08-28 09:31:00 -0500

    Inicializamos el Proyecto

---
+++ src/gamermaniamx/GamermaniaMX.java
@@ -0,0 +1,15 @@
+/*
+Autor Mao rivera Sanchez Armas
+Creado para GamerManiaMX
+*/
+package gamermaniamx;
+
+
+public class GamermaniaMX {
+
+    
+    public static void main(String[] args) {
+      
+    }
+    
+}

13af54936d0c0354e4a8527f9f990b9fb65f2787
Mao Rivera <Mao Rivera@ba-dd2dbde06924>
2016-08-28 11:49:44 -0500

    Empezamos con las primeras ventanas y preparando el candado de no abrir mas ventanas

--- src/gamermaniamx/GamermaniaMX.java
+++ src/gamermaniamx/GamermaniaMX.java
@@ -4,12 +4,18 @@ Creado para GamerManiaMX
 */
 package gamermaniamx;
 
+import javax.swing.JFrame;
+
 
 public class GamermaniaMX {
 
     
     public static void main(String[] args) {
-      
+      //Instanciamos la portada y la maximizamos
+        Portada portada=new Portada();
+        portada.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        portada.setDefaultCloseOperation(portada.EXIT_ON_CLOSE);
+        portada.setVisible(true);
     }
     
 }
